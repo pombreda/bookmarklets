a068cee67a37d359b75689f4b34391dd02bac56a
Ramiro Gomez <web@ramiro.org>
2012-12-13 20:40:13 +0100

    clarify differences with version from marklets.com

--- datatables/tables2csv.js
+++ datatables/tables2csv.js
@@ -1,29 +1,33 @@
 javascript: (function() {
-// generic data table to csv converter
-// http://net.tutsplus.com/tutorials/javascript-ajax/create-bookmarklets-the-right-way/
+/*
+generic HTML data table to CSV converter adapted from:
+http://marklets.com/CSV+from+HTML+tables.aspx
+
+The code below uses a comma instead of a semi-colon and automatically loads
+jQuery if not already loaded.
+*/
 if ('undefined'==typeof jQuery) {
-    script = document.createElement( 'script' );
-    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
-    script.onload=tables2CSV;
-    document.body.appendChild(script);
+  script = document.createElement( 'script' );
+  script.src = 'http://code.jquery.com/jquery-1.8.3.min.js';
+  script.onload=tables2CSV;
+  document.body.appendChild(script);
 }
 else {
-    tables2CSV();
+  tables2CSV();
 }
 
-// http://marklets.com/CSV+from+HTML+tables.aspx
 function tables2CSV() {
-   $('table').each(function() {
-      var $table = $(this);
-      $('<div/>')
-        .css('border', $table.css('border'))
-        .css('width', $table.width())
-        .html($.map($table.find('tr'), function(tr) {
-            return $.map($(tr).find('th, td'), function(e) {
-                return '"' + $(e).text().trim().replace('"', '""') + '"'
-           }).join(',')
-        }).join('<br>')).insertAfter($table);
-   });
+  $('table').each(function() {
+    var $table = $(this);
+    $('<div/>')
+      .css('border', $table.css('border'))
+      .css('width', $table.width())
+      .html($.map($table.find('tr'), function(tr) {
+        return $.map($(tr).find('th, td'), function(e) {
+          return '"' + $(e).text().trim().replace('"', '""') + '"'
+        }).join(',')
+      }).join('<br>')).insertAfter($table);
+  });
 }
 
 })();

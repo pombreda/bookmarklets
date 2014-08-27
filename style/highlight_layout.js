e03c6ac40b17765bac9a8d0786de9a214af1a688
Ramiro Gómez <code@ramiro.org>
2014-08-27 20:12:46 +0200

    added comment with source

--- style/highlight_layout.js
+++ style/highlight_layout.js
@@ -1,3 +1,8 @@
+/*
+ Add different background colors to elements based on depth in the DOM tree
+ to highlight the layout of the page, based on:
+ http://www.quora.com/Web-Development/What-are-the-most-interesting-HTML-JS-DOM-CSS-hacks-that-most-web-developers-dont-know-about/answer/Gajus-Kuizinas
+*/
 javascript:(function(){
     var css = document.createElement('style');
     css.type = 'text/css';
